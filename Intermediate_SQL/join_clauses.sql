09b71e5beab62f4d65639f1229c92c1899d1c2dd
ShahriarHA <shahriar08neub@gmail.com>
2025-03-24 00:34:52 +0600

    Join clause

---
+++ Intermediate_SQL/join_clauses.sql
@@ -0,0 +1,35 @@
+select * from world.city;
+select * from world.country;
+
+-- INNER JOIN;
+select wc.Code,wc.Name,wcl.Language from world.country as wc
+inner join world.countrylanguage as wcl
+on wc.Code = wcl.CountryCode;
+
+-- LEFT JOIN 
+select wci.Name,wci.CountryCode,wcl.Language from world.city as wci
+left join world.countrylanguage as wcl
+on wci.CountryCode = wcl.CountryCode
+order by wci.Name;
+
+-- RIGHT JOIN: he RIGHT JOIN keyword returns all records from the right table, and the matching records from the left table. 
+select wci.Name,wci.CountryCode,wcl.Language from world.city as wci
+right join world.countrylanguage as wcl
+on wci.CountryCode = wcl.CountryCode
+order by wci.Name;
+
+-- SELF JOIN
+select wc.ID as world_city_id,wc.Name as world_city_name,wc1.ID,wc1.Name,wc1.CountryCode 
+from world.city as wc
+join world.city as wc1
+on wc.ID+1 = wc1.ID
+
+ 
+
+
+
+
+
+
+
+ 
